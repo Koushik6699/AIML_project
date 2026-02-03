305c50ff275995f8ae3471ed8ea9b07b4f4f941f
Koushik M <koushik.m@s.amity.edu>
2026-02-03 09:04:16 +0530

    edited js added live deployed backend link

--- frontend/script.js
+++ frontend/script.js
@@ -5,7 +5,7 @@
 // ===================== CONFIG =====================
 
 // Change this when deploying
-const BACKEND_BASE_URL = "http://127.0.0.1:5000";
+const BACKEND_BASE_URL = "https://aiml-project-ascp.onrender.com";
 
 // ===================== SUBJECT DATABASE =====================
 
